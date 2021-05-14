0ff9dc37a50a7d56a1f9608ed28a35e9de45cab9
Federico Alvarez <fagomezomil@gmail.com>
2021-05-14 11:42:00 -0300

    componente buscador categorias

---
+++ src/components/BuscadorCategorias.jsx
@@ -0,0 +1,9 @@
+
+
+export default function BuscadorCategorias() {
+  return (
+    <>
+        vale por sellecion por categorias y un buscador
+    </>
+  );
+}
--- src/pages/Computadoras.jsx
+++ src/pages/Computadoras.jsx
@@ -5,6 +5,7 @@ import slideCompu3 from "../assets/img/slidecompus3.png"
 import compu2 from "../assets/img/compu2.jpeg"
 import { Carousel } from "react-bootstrap"
 import SeccionEnviosPagos from "../components/SeccionEnviosPagos"
+import BuscadorCategorias from "../components/BuscadorCategorias"
 
 export default function Computadoras() {
     return (
@@ -26,7 +27,7 @@ export default function Computadoras() {
 
 
                 <div className="col-12 contenedor-items pb-4">
-
+                    <BuscadorCategorias />
                     <div className="row">
                         <div className="col-3 my-1 p-4">
                             <button className="btn-serie">Serie Advance</button>
@@ -92,75 +93,6 @@ export default function Computadoras() {
                             <span> <button className="btn-cart"> <i class="fas fa-shopping-cart"></i></button></span>
                             <button className="btn-comprar mt-2" >Comprar</button>
                         </div>
-                        <div className="col-3 my-1 p-4">
-                            <button className="btn-serie">Serie Advance</button>
-                            <img src={compu2} alt="" className="img-fluid" />
-                            <p className="titulo-producto">Estación de trabajo HP Z2 Mini G4</p>
-                            <ul className="detalle-producto">
-                                <li>Procesador AMD Ryzen™ 3</li>
-                                <li>Windows 10 Home 64</li>
-                                <li>4 GB de SDRAM DDR4-2400 (1 x 4 GB)</li>
-                                <li>Unidad de estado sólido PCIe® NVMe™ M.2 de 256 GB</li>
-                                <li>Gráficos AMD Radeon™</li>
-                            </ul>
-                            <span className="precio-producto">$64.000</span>
-                            <span> <button className="btn-heart"> <i class="fas fa-heart"></i></button></span>
-                            <span> <button className="btn-cart"> <i class="fas fa-shopping-cart"></i></button></span>
-                            <button className="btn-comprar mt-2" >Comprar</button>
-                        </div>
-                        <div className="col-3 my-1 p-4">
-                            <button className="btn-serie">Serie Advance</button>
-                            <img src={compu2} alt="" className="img-fluid" />
-                            <p className="titulo-producto">Estación de trabajo HP Z2 Mini G4</p>
-                            <ul className="detalle-producto">
-                                <li>Procesador AMD Ryzen™ 3</li>
-                                <li>Windows 10 Home 64</li>
-                                <li>4 GB de SDRAM DDR4-2400 (1 x 4 GB)</li>
-                                <li>Unidad de estado sólido PCIe® NVMe™ M.2 de 256 GB</li>
-                                <li>Gráficos AMD Radeon™</li>
-                            </ul>
-                            <span className="precio-producto">$64.000</span>
-                            <span> <button className="btn-heart"> <i class="fas fa-heart"></i></button></span>
-                            <span> <button className="btn-cart"> <i class="fas fa-shopping-cart"></i></button></span>
-                            <button className="btn-comprar mt-2" >Comprar</button>
-                        </div>
-                        <div className="col-3 my-1 p-4">
-                            <button className="btn-serie">Serie Advance</button>
-                            <img src={compu2} alt="" className="img-fluid" />
-                            <p className="titulo-producto">Estación de trabajo HP Z2 Mini G4</p>
-                            <ul className="detalle-producto">
-                                <li>Procesador AMD Ryzen™ 3</li>
-                                <li>Windows 10 Home 64</li>
-                                <li>4 GB de SDRAM DDR4-2400 (1 x 4 GB)</li>
-                                <li>Unidad de estado sólido PCIe® NVMe™ M.2 de 256 GB</li>
-                                <li>Gráficos AMD Radeon™</li>
-                            </ul>
-                            <span className="precio-producto">$64.000</span>
-                            <span> <button className="btn-heart"> <i class="fas fa-heart"></i></button></span>
-                            <span> <button className="btn-cart"> <i class="fas fa-shopping-cart"></i></button></span>
-                            <button className="btn-comprar mt-2" >Comprar</button>
-                        </div>
-                        <div className="col-3 my-1 p-4">
-                            <button className="btn-serie">Serie Advance</button>
-                            <img src={compu2} alt="" className="img-fluid" />
-                            <p className="titulo-producto">Estación de trabajo HP Z2 Mini G4</p>
-                            <ul className="detalle-producto">
-                                <li>Procesador AMD Ryzen™ 3</li>
-                                <li>Windows 10 Home 64</li>
-                                <li>4 GB de SDRAM DDR4-2400 (1 x 4 GB)</li>
-                                <li>Unidad de estado sólido PCIe® NVMe™ M.2 de 256 GB</li>
-                                <li>Gráficos AMD Radeon™</li>
-                            </ul>
-                            <span className="precio-producto">$64.000</span>
-                            <span> <button className="btn-heart"> <i class="fas fa-heart"></i></button></span>
-                            <span> <button className="btn-cart"> <i class="fas fa-shopping-cart"></i></button></span>
-                            <button className="btn-comprar mt-2" >Comprar</button>
-                        </div>
-                        
-                        
-                        
-                        
-                        
                     </div>
                     
 
