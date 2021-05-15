e9584e17c1981e598a1b562b47fcba54692f61e3
Federico Alvarez <fagomezomil@gmail.com>
2021-05-15 14:54:09 -0300

    ajustes mdbreact

--- src/components/BuscadorCategorias.jsx
+++ src/components/BuscadorCategorias.jsx
@@ -4,13 +4,13 @@ export default function BuscadorCategorias() {
   return (
 
         <div className="row container-buscador">
-            <div className="col-5">
+            <div className="col-6">
                 <span className="texto-categoria">Elija por categoría:</span>
                 <button className="btn-categoria my-2 mx-2">Serie Advance</button>
                 <button className="btn-categoria my-2 mx-2">Serie Office</button>
                 <button className="btn-categoria my-2 mx-2">Serie Home</button>
             </div>
-            <div className="col-7">
+            <div className="col-6">
                 <form>
                     <label htmlFor="" className="texto-categoria me-2">Buscar</label>
                     <input type="text" name="buscar" id="" style={{width:'60%'}} className="input-buscar"/>
