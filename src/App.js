c166717e62b24e1ac9f47bcb135a8a39d7b52b60
tomas <tomas_053@hotmail.es>
2021-05-17 17:24:41 -0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+
+
 import "./App.css";
+import "./Admin.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavReactB from "./components/NavReactB";
+import NavAdmin from "./components/NavAdmin";
+import ProductosAdmin from "./components/ProductosAdmin";
+import UsuariosAdmin from "./components/UsuariosAdmin";
+import EstadisticasAdmin from "./components/EstadisticasAdmin";
 import Footer from "./components/Footer";
 import Inicio from "./pages/Inicio.jsx";
 import Computadoras from "./pages/Computadoras";
@@ -8,22 +15,35 @@ import Producto from "./pages/Producto";
 import Login from "./pages/Login";
 import Registro from "./pages/Registro";
 
+
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <NavReactB />
       <Switch>
         <Route path="/inicio">
+          <NavReactB />
           <Inicio />
+          <Footer />
         </Route>
         <Route path="/computadoras">
+          <NavReactB />
           <Computadoras />
+          <Footer />
         </Route>
-        <Route path="/notebooks"></Route>
-        <Route path="/accesorios"></Route>
-        <Route path="/nosotros"></Route>
+        <Route path="/notebooks">
+          <NavReactB />
+          <Footer />
+        </Route>
+        <Route path="/accesorios">
+          <NavReactB />
+          <Footer />
+        </Route>
+        <Route path="/nosotros">
+          <NavReactB />
+          <Footer />
+        </Route>
         <Route path="/producto">
           <Producto />
         </Route>
@@ -33,8 +53,19 @@ function App() {
         <Route path="/registro">
           <Registro />
         </Route>
+        <Route path="/productos">
+          <NavAdmin />
+          <ProductosAdmin />
+        </Route>
+        <Route path="/usuarios">
+          <NavAdmin />
+          <UsuariosAdmin />
+        </Route>
+        <Route path="/estadisticas">
+          <NavAdmin />
+          <EstadisticasAdmin />
+        </Route>
       </Switch>
-      <Footer />
     </Router>
   );
 }
