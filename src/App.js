525ff1ade5fce21fe938722b9a6e7175182f64f1
tomas <tomas_053@hotmail.es>
2021-05-17 18:49:28 -0300

    no mas /inicio

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/inicio">
+        <Route path="/">
           <NavReactB />
           <Inicio />
           <Footer />
