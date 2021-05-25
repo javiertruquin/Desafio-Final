4e005f0108bbeb894258f3f08e62775e88fdf16d
Federico Alvarez <fagomezomil@gmail.com>
2021-05-25 15:59:16 -0300

    ajustes nav admin y producto...

--- src/components/NavAdmin.jsx
+++ src/components/NavAdmin.jsx
@@ -1,11 +1,24 @@
 import { Navbar, Nav } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+
+import logoW from "../assets/img/logoW.png";
 
 export default function NavAdmin() {
   return (
-    <Navbar fixed="top" bg="dark" variant="dark">
-      <div className="container justify-content-start">
-        {/* <Navbar.Brand href="/home" className="col-1"><Image className="w-100" src={logo} rounded /></Navbar.Brand> */}
-        <Nav style={{width: "100%"}} className="text-center" variant="tabs" defaultActiveKey="/home">
+
+<Navbar collapseOnSelect expand="lg"  bg="dark" variant="dark" className="sticky-top">
+      <div className="container">
+        <Navbar.Brand to="/inicio" as={NavLink} className="px-4">
+          <img
+            src={logoW}
+            height="50px"
+            className="d-inline-block align-top "
+            alt="Logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto condensed">
           <Nav.Item className="col">
             <Nav.Link href="/productos">Productos</Nav.Link>
           </Nav.Item>
@@ -15,8 +28,34 @@ export default function NavAdmin() {
           <Nav.Item className="col">
             <Nav.Link href="/estadisticas" eventKey="link-2">Estadísticas</Nav.Link>
           </Nav.Item>
-        </Nav>
+            
+          <Nav.Item className="col">
+              <Nav.Link to="/login" as={NavLink}>
+                <i style={{ color: "#00a8db" }} class="fas fa-user"></i>
+              </Nav.Link>
+              </Nav.Item>
+
+          </Nav>
+
+        </Navbar.Collapse>
       </div>
     </Navbar>
+
+    // <Navbar fixed="top" bg="dark" variant="dark">
+    //   <div className="container justify-content-start">
+    //     {/* <Navbar.Brand href="/home" className="col-1"><Image className="w-100" src={logo} rounded /></Navbar.Brand> */}
+    //     <Nav style={{width: "100%"}} className="text-center" variant="tabs" defaultActiveKey="/home">
+    //       <Nav.Item className="col">
+    //         <Nav.Link href="/productos">Productos</Nav.Link>
+    //       </Nav.Item>
+    //       <Nav.Item className="col">
+    //         <Nav.Link href="/usuarios" eventKey="link-1">Usuarios</Nav.Link>
+    //       </Nav.Item>
+    //       <Nav.Item className="col">
+    //         <Nav.Link href="/estadisticas" eventKey="link-2">Estadísticas</Nav.Link>
+    //       </Nav.Item>
+    //     </Nav>
+    //   </div>
+    // </Navbar>
   );
 }
--- src/components/ProductosAdmin.jsx
+++ src/components/ProductosAdmin.jsx
@@ -4,23 +4,35 @@ import CardProductoAdmin from "./CardProductoAdmin";
 import FormEditar from "./FormEditar";
 
 export default function ProductosAdmin() {
-  const [crear, setCrear  ] = useState(false);
+  const [crear, setCrear] = useState(false);
 
-  const handleClose = () => setCrear  (false);
-  const handleCrear = () => setCrear  (true);
+  const handleClose = () => setCrear(false);
+  const handleCrear = () => setCrear(true);
 
   return (
     <div>
-      <Container className="mt-5">
-        <div className="text-center py-5">
-          <Button
-            className="py-3 px-5"
-            variant="secondary"
-            onClick={handleCrear}
-          >
-            Agregar Producto
-          </Button>{" "}
+      <Container className="">
+        <div className="row bg-fondo-producto-admin img-fluid">
+          <div className="col-6">
+            <span className="titulo-dash">Click aquí para crear un producto</span>
+            
+          </div>
+          <div className="col-6 text-center py-5">
+            <div className=" mx-5"><button onClick={handleCrear} className="btn-dash">
+            <i class="fas fa-plus-square"></i> Agregar producto
+            </button></div>
+            
+            {/* <Button
+              className="py-3 px-5"
+              variant="secondary"
+              onClick={handleCrear}
+            >
+              Agregar Producto
+          </Button>{" "} */}
+          </div>
+
         </div>
+
 
         <CardProductoAdmin />
         <CardProductoAdmin />
@@ -34,7 +46,7 @@ export default function ProductosAdmin() {
           <Modal.Title>Producto</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-            <FormEditar />
+          <FormEditar />
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
