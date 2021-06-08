33772f790fa55a7f5959fe283a92d1ec419f6d38
tomas <tomas_053@hotmail.es>
2021-06-08 17:19:54 -0300

    merge manual

--- src/App.js
+++ src/App.js
@@ -28,97 +28,116 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 
-function App() {
-    const [user, setUser] = useState("");
-    const [token, setToken] = useState("");
+const localToken = JSON.parse(localStorage.getItem("token"))?.token || "";
 
-    return (
-        <Router>
-            <Route>
-                <ScrollToTop></ScrollToTop>
-            </Route>
-            <Switch>
-                <Route path="/" exact>
-                    <NavReactB />
-                    <Inicio />
-                    <Footer />
-                </Route>
-                <Route path="/computadoras">
-                    <NavReactB />
-                    <Computadoras />
-                    <Footer />
-                </Route>
-                <Route path="/notebooks">
-                    <NavReactB />
-                    <Notebooks />
-                    <Footer />
-                </Route>
-                <Route path="/accesorios">
-                    <NavReactB />
-                    <Accesorios />
-                    <Footer />
-                </Route>
-                <Route path="/nosotros">
-                    <NavReactB />
-                    <SobreNosotros />
-                    <Footer />
-                </Route>
-                <Route path="/producto">
-                    <Producto />
-                </Route>
-                <Route path="/carrito">
-                    <Carrito />
-                </Route>
-                <Route path="/envio">
-                    <Envio />
-                </Route>
-                <Route path="/pago">
-                    <Pago />
-                </Route>
-                <Route path="/login">
-                    <NavReactB />
-                    <Login setUser={setUser} setToken={setToken} />
-                    <Footer />
-                </Route>
-                <Route path="/registro">
-                    <NavReactB />
-                    <Registro setToken={setToken} />
-                    <Footer />
-                </Route>
-                <Route path="/productos">
-                    <NavAdmin />
-                    <ProductosAdmin />
-                </Route>
-                <Route path="/usuarios">
-                    <NavAdmin />
-                    <UsuariosAdmin />
-                </Route>
-                <Route path="/tucuman">
-                    <NavReactB />
-                    <Tucuman />
-                </Route>
-                <Route path="/capital">
-                    <NavReactB />
-                    <Capital />
-                </Route>
-                <Route path="/rosario">
-                    <NavReactB />
-                    <Rosario />
-                </Route>
-                <Route path="/sannicolas">
-                    <NavReactB />
-                    <Sannicolas />
-                </Route>
-                <Route path="/estadisticas">
-                    <NavAdmin />
-                    <EstadisticasAdmin />
-                </Route>
-            </Switch>
-        </Router>
-    );
+function App() {
+  const [user, setUser] = useState("");
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    if (token) {
+      const request = async () => {
+        axios.defaults.headers = { "x-auth-token": token };
+        const { data } = await axios.get("/auth");
+        setUser(data);
+      };
+      request();
+    }
+  }, [token]);
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    axios.defaults.headers = { "x-auth-token": "" };
+    setUser({});
+    setToken("");
+  };
+
+  // Para hacer el logout
+
+  // const logout = () => {
+  //     localStorage.removeItem('token');
+  //     axios.defaults.headers = { 'x-auth-token': '' };
+  //     setUser({});
+  //     setToken('');
+  // };
+
+  return (
+    <Router>
+      <Route>
+        <ScrollToTop></ScrollToTop>
+      </Route>
+      {user.roll === "admin" || user.roll === "vendedor" ? (
+        <NavAdmin />
+      ) : (
+        <NavReactB />
+      )}
+      <Switch>
+        <Route path="/" exact>
+          <Inicio />
+          <Footer />
+        </Route>
+        <Route path="/computadoras">
+          <Computadoras />
+          <Footer />
+        </Route>
+        <Route path="/notebooks">
+          <Notebooks />
+          <Footer />
+        </Route>
+        <Route path="/accesorios">
+          <Accesorios />
+          <Footer />
+        </Route>
+        <Route path="/nosotros">
+          <SobreNosotros />
+          <Footer />
+        </Route>
+        <Route path="/producto">
+          <Producto />
+        </Route>
+        <Route path="/carrito">
+          <Carrito />
+        </Route>
+        <Route path="/envio">
+          <Envio />
+        </Route>
+        <Route path="/pago">
+          <Pago />
+        </Route>
+        <Route path="/login">
+          <Login setUser={setUser} setToken={setToken} />
+          <Footer />
+        </Route>
+        <Route path="/registro">
+          <Registro setToken={setToken} />
+          <Footer />
+        </Route>
+        <Route path="/productos">
+          <ProductosAdmin />
+        </Route>
+        <Route path="/usuarios">
+          <UsuariosAdmin />
+        </Route>
+        <Route path="/tucuman">
+          <Tucuman />
+        </Route>
+        <Route path="/capital">
+          <Capital />
+        </Route>
+        <Route path="/rosario">
+          <Rosario />
+        </Route>
+        <Route path="/sannicolas">
+          <Sannicolas />
+        </Route>
+        <Route path="/estadisticas">
+          <EstadisticasAdmin />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
