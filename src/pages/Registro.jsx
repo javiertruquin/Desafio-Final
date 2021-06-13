d7c1b05fc7c29b26bf5633826bb63f7987799f83
Andres <andresj_077@hotmail.com>
2021-06-13 01:57:02 -0300

    mas de mensajes

--- src/pages/Registro.jsx
+++ src/pages/Registro.jsx
@@ -1,149 +1,137 @@
-import axios from "axios";
 import React from "react";
-import { Form } from "react-bootstrap";
-import { MDBInput } from "mdbreact";
-import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
-import { useState } from "react";
-import { Link, useHistory } from "react-router-dom";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardHeader,
+  MDBInput,
+  MDBBtn,
+  MDBTable,
+  MDBTableBody,
+  MDBTableHead,
+} from "mdbreact";
 
-export default function Registro({ setToken }) {
-    const [validated, setValidated] = useState(false);
-    const [input, setInput] = useState({});
-    const history = useHistory();
+const Mensaje = (props) => {
+  const data_panel = {
+    columns: [
+      {
+        label: <MDBInput label=" " type="checkbox" id="checkbox5" />,
+        field: "check",
+        sort: "asc",
+      },
+      {
+        label: "First Name",
+        field: "first",
+        sort: "asc",
+      },
+      {
+        label: "Last Name",
+        field: "last",
+        sort: "asc",
+      },
+      {
+        label: "Username",
+        field: "username",
+        sort: "asc",
+      },
+      {
+        label: "Username",
+        field: "username2",
+        sort: "asc",
+      },
+      {
+        label: "Username",
+        field: "username3",
+        sort: "asc",
+      },
+      {
+        label: "Username",
+        field: "username4",
+        sort: "asc",
+      },
+    ],
+    rows: [
+      {
+        check: <MDBInput label=" " type="checkbox" id="checkbox6" />,
+        first: "Mark",
+        last: "Otto",
+        username: "@mdo",
+        username2: "Mark",
+        username3: "Otto",
+        username4: "@mdo",
+      },
+      {
+        check: <MDBInput label=" " type="checkbox" id="checkbox7" />,
+        first: "Jacob",
+        last: "Thornton",
+        username: "@fat",
+        username2: "Jacob",
+        username3: "Thornton",
+        username4: "@fat",
+      },
+      {
+        check: <MDBInput label=" " type="checkbox" id="checkbox8" />,
+        first: "Larry",
+        last: "the Bird",
+        username: "@twitter",
+        username2: "Larry",
+        username3: "the Bird",
+        username4: "@twitter",
+      },
+      {
+        check: <MDBInput label=" " type="checkbox" id="checkbox9" />,
+        first: "Paul",
+        last: "Topolski",
+        username: "@P_Topolski",
+        username2: "Paul",
+        username3: "Topolski",
+        username4: "@P_Topolski",
+      },
+      {
+        check: <MDBInput label=" " type="checkbox" id="checkbox10" />,
+        first: "Larry",
+        last: "the Bird",
+        username: "@twitter",
+        username2: "Larry",
+        username3: "the Bird",
+        username4: "@twitter",
+      },
+    ],
+  };
 
-    const handleSubmit = async (event) => {        
-        const form = event.currentTarget;
-        event.preventDefault();
-        setValidated(true);
-        if (form.checkValidity() === false) {
-            return event.stopPropagation();
-        }
-        try {
-            const { data } = await axios.post("/auth/register", input);
-            localStorage.setItem("token", JSON.stringify(data));
-            setToken(data.token);
-            history.push("/");
-            // window.location.replace('/');
-        } catch (error) {
-            console.log(error.response.data);
-        }
-    };
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        let changedInput = { ...input, [name]: value };
-        setInput({ ...changedInput, rol: "usuario" });
-    };
+  return (
+    <MDBCard narrow>
+      <MDBCardHeader className="view view-cascade gradient-card-header blue-gradient d-flex justify-content-between align-items-center py-2 mx-4 mb-3">
+        <div>
+          <MDBBtn outline rounded size="sm" color="white" className="px-2">
+            <i className="fa fa-th-large mt-0"></i>
+          </MDBBtn>
+          <MDBBtn outline rounded size="sm" color="white" className="px-2">
+            <i className="fa fa-columns mt-0"></i>
+          </MDBBtn>
+        </div>
+        <a href="#" className="white-text mx-3">
+          Table name
+        </a>
+        <div>
+          <MDBBtn outline rounded size="sm" color="white" className="px-2">
+            <i className="fas fa-pencil-alt mt-0"></i>
+          </MDBBtn>
+          <MDBBtn outline rounded size="sm" color="white" className="px-2">
+            <i className="fas fa-times mt-0"></i>
+          </MDBBtn>
+          <MDBBtn outline rounded size="sm" color="white" className="px-2">
+            <i className="fa fa-info-circle mt-0"></i>
+          </MDBBtn>
+        </div>
+      </MDBCardHeader>
+      <MDBCardBody cascade>
+        <MDBTable btn fixed>
+          <MDBTableHead columns={data_panel.columns} />
+          <MDBTableBody rows={data_panel.rows} />
+        </MDBTable>
+      </MDBCardBody>
+    </MDBCard>
+  );
+};
 
-    return (
-        <Form
-            noValidate
-            validated={validated}
-            onSubmit={handleSubmit}
-            style={{ width: "777px" }}
-            className="container card mx-auto pt-2 pr-4 pl-4 mt-4"
-        >
-            <h2
-                style={{ width: "421px" }}
-                className="mx-auto text-center p-4 mb-2 titulo-azul"
-            >
-                Ingresá tus datos
-            </h2>
-            <MDBContainer className="mb-4">
-                <MDBRow>
-                    <MDBCol>
-                        <MDBInput
-                            maxlength="30"
-                            name="nombre"
-                            onChange={(e) => handleChange(e)}
-                            required
-                            type="text"
-                            label="Nombre/s"
-                        >
-                            <div className="invalid-feedback">
-                                Ingrese su nombre aquí
-                            </div>
-                        </MDBInput>
-                    </MDBCol>
-                    <MDBCol>
-                        <MDBInput
-                            maxlength="30"
-                            name="apellido"
-                            onChange={(e) => handleChange(e)}
-                            required
-                            type="text"
-                            label="Apellido"
-                        >
-                            <div className="invalid-feedback">
-                                Ingrese su apellido aquí
-                            </div>
-                        </MDBInput>
-                    </MDBCol>
-                </MDBRow>
-
-                <MDBRow>
-                    <MDBCol>
-                        <MDBInput
-                            maxlength="50"
-                            name="email"
-                            onChange={(e) => handleChange(e)}
-                            required
-                            type="email"
-                            label="E-mail"
-                        >
-                            <div className="invalid-feedback">
-                                Ingrese su e-mail aquí
-                            </div>
-                        </MDBInput>
-                    </MDBCol>
-                    <MDBCol></MDBCol>
-                </MDBRow>
-                <MDBRow>
-                    <MDBCol>
-                        <MDBInput
-                            minLength="6"
-                            maxlength="30"
-                            name="password"
-                            // onChange={(e) => handleChange(e)}
-                            type="password"
-                            aria-describedby="inputGroupPrepend"
-                            required
-                            label="Contraseña"
-                            id="pass1"
-                        >
-                            <div className="invalid-feedback">
-                                Ingrese una contraseña de entre 6 y 30 caracteres
-                            </div>
-                        </MDBInput>
-                    </MDBCol>
-                    <MDBCol>
-                        <MDBInput
-                            minLength="6"
-                            maxlength="30"
-                            name="password"
-                            onChange={(e) => handleChange(e)}
-                            type="password"
-                            aria-describedby="inputGroupPrepend"
-                            required
-                            label="Confirmar Contraseña"
-                            id="pass2"
-                            >
-                            <div className="invalid-feedback">
-                                Ingrese nuevamente la contraseña aquí
-                            </div>
-                        </MDBInput>
-                    </MDBCol>
-                </MDBRow>
-            </MDBContainer>
-            <button
-                type="submit"
-                style={{ width: "40%" }}
-                className="mx-auto mt-4 mb-2 btn-categoria"
-            >
-                REGISTRARSE
-            </button>
-            <p className="mt-1 mb-5 text-center advertencia-registro">
-                Asegurate de tener acceso al e-mail que ingresaste
-            </p>
-        </Form>
-    );
-}
+export default Mensaje;
