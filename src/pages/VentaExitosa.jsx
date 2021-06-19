5594f4d71837e54b62d92ecda0266d1cab6675cd
javier Truquin <javiertruquin@gmail.com>
2021-06-19 19:07:46 -0300

    pantalla de exito

--- src/pages/VentaExitosa.jsx
+++ src/pages/VentaExitosa.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Exito from "../Exito.css";
 
 export default function VentaExitosa() {
     return (
         <div>
-            EXITO
+            <div className="modal-dialog modal-confirm">
+                <div className="modal-content fuente">
+                    <div className="modal-header">
+                        <div className="icon-box">
+                            <i class="fas fa-check"></i>
+                        </div>
+                        <h4 className="modal-title w-100">Awesome!</h4>
+                    </div>
+                    <div className="modal-body">
+                        <p className="text-center">
+                            Your booking has been confirmed. Check your email
+                            for detials.
+                        </p>
+                    </div>
+                    <div className="modal-footer">
+                        <button
+                            className="btn btn-success btn-block"
+                            data-dismiss="modal"
+                        >
+                            OK
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
-
